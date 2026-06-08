242bf17672a5e263cd86b5a9f0f35d08d49ee9df
Nikita <miracleqxz@gmail.com>
2026-06-08 21:07:58 +0300

    Initail commit + dependencies pom.xml

---
+++ src/main/java/com/tgbot/prodaction/ProdactionApplication.java
@@ -0,0 +1,13 @@
+package com.tgbot.prodaction;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProdactionApplication {
+
+  public static void main(String[] args) {
+    SpringApplication.run(ProdactionApplication.class, args);
+  }
+
+}
---
+++ src/test/java/com/tgbot/prodaction/ProdactionApplicationTests.java
@@ -0,0 +1,13 @@
+package com.tgbot.prodaction;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProdactionApplicationTests {
+
+  @Test
+  void contextLoads() {
+  }
+
+}
