7bd3e98268e2e514726d842693c9c250423f35c1
Nikita <miracleqxz@gmail.com>
2026-06-17 21:22:27 +0300

    feature: add Telegram bot with start command

--- src/main/java/com/tgbot/prodaction/bot/TelegramReminderBot.java
+++ src/main/java/com/tgbot/prodaction/bot/TelegramReminderBot.java
@@ -8,19 +8,35 @@ import org.springframework.stereotype.Component;
 import org.telegram.telegrambots.longpolling.interfaces.LongPollingUpdateConsumer;
 import org.telegram.telegrambots.longpolling.starter.SpringLongPollingBot;
 import org.telegram.telegrambots.longpolling.util.LongPollingSingleThreadUpdateConsumer;
+import org.telegram.telegrambots.meta.api.methods.send.SendMessage;
 import org.telegram.telegrambots.meta.api.objects.Update;
+import org.telegram.telegrambots.meta.exceptions.TelegramApiException;
+import org.telegram.telegrambots.meta.generics.TelegramClient;
+import org.telegram.telegrambots.client.okhttp.OkHttpTelegramClient;
 
 @Component
 public class TelegramReminderBot implements SpringLongPollingBot,
     LongPollingSingleThreadUpdateConsumer {
   private static final Logger log = LoggerFactory.getLogger(TelegramReminderBot.class);
+  private final static String START_COMMAND = "/start";
+  private static final String START_MESSAGE = """
+      Привет! Я бот для напоминаний.
+
+      Чтобы создать напоминание, напиши:
+      /remind 15.06.2026 19:30 текст напоминания
+      """;
+  private final static String UNKNOWN_START_COMMAND = "Пока что я понимаю только текстовые сообщения";
+
+
   private final TelegramBotProperties telegramBotProperties;
   private final TelegramUserService telegramUserService;
+  private final TelegramClient tgClient;
 
   public TelegramReminderBot(TelegramBotProperties telegramBotProperties,
       TelegramUserService telegramUserService){
     this.telegramBotProperties = telegramBotProperties;
     this.telegramUserService = telegramUserService;
+    this.tgClient = new OkHttpTelegramClient(telegramBotProperties.getToken());
   }
 
   @Override
@@ -41,23 +57,66 @@ public class TelegramReminderBot implements SpringLongPollingBot,
     }
     if (!update.getMessage().hasText()) {
       log.debug("Message without text from {} with type: {}", update.getMessage().getFrom().getFirstName(), getMessageType(update));
+      return;
     }
-    //sendMessage
+    Long chatId = update.getMessage().getChatId();
+    String messageText = update.getMessage().getText();
+
+    if (START_COMMAND.equals(messageText)) {
+      handleStartCommand(update);
+      return;
+    }
+    sendMessage(chatId, UNKNOWN_START_COMMAND);
   }
 
-  private void SendMessage(Long chatId, String text) {
+  public void handleStartCommand(Update update) {
+    Long chatId = update.getMessage().getChatId();
 
+    telegramUserService.findOrCreateUser(update.getMessage().getFrom().getId(), chatId, update.getMessage().getFrom().getUserName(),
+        update.getMessage().getFrom().getFirstName(), update.getMessage().getFrom().getLastName());
+
+    sendMessage(chatId, START_MESSAGE);
+  }
+
+  private void sendMessage(Long chatId, String text) {
+    SendMessage message = SendMessage
+        .builder()
+        .chatId(chatId)
+        .text(text)
+        .build();
+
+    try {
+      tgClient.execute(message);
+    } catch (TelegramApiException e) {
+      log.error("Failed to send message to chat id: {}", chatId, e);
+    }
   }
 
   private String getMessageType(Update update){
-    if (update.getMessage().hasPhoto()) return "фото";
-    if (update.getMessage().hasVideo()) return "видео";
-    if (update.getMessage().hasSticker()) return "стикер";
-    if (update.getMessage().hasAudio()) return "аудио";
-    if (update.getMessage().hasVoice()) return "голосовое";
-    if (update.getMessage().hasDocument()) return "документ";
-    if (update.getMessage().hasLocation()) return "локация";
-    if (update.getMessage().hasContact()) return "контакт";
+    if (update.getMessage().hasPhoto()) {
+      return "фото";
+    }
+    if (update.getMessage().hasVideo()) {
+      return "видео";
+    }
+    if (update.getMessage().hasSticker()) {
+      return "стикер";
+    }
+    if (update.getMessage().hasAudio()) {
+      return "аудио";
+    }
+    if (update.getMessage().hasVoice()) {
+      return "голосовое";
+    }
+    if (update.getMessage().hasDocument()) {
+      return "документ";
+    }
+    if (update.getMessage().hasLocation()) {
+      return "локация";
+    }
+    if (update.getMessage().hasContact()) {
+      return "контакт";
+    }
     return "неизвестный тип";
   }
 }
